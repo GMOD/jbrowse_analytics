34b11f7e46276230ac9437ecad0f7efe602eeb4c
Robert Buels <rbuels@gmail.com>
2012-06-18 20:06:58 +0000

    just use pgrep and pkill for init script machinery directly

--- init_script.sh
+++ init_script.sh
@@ -9,7 +9,6 @@
 
 # Creamos un fichero PID para monit
 SCRIPT="JBrowseAnalytics.js"
-PIDFILE="/var/run/jbrowse_analytics.pid"
 
 USER="jb_analytics"
 PGREP_ARGS="-u $USER -f $SCRIPT"
@@ -18,35 +17,29 @@ ROOT_DIR="/var/www/jbrowse_analytics"
 
 SERVER="JBrowse analytics"
 LOG_FILE="$ROOT_DIR/JBrowseAnalytics.log"
-LOCK_FILE="/var/lock/subsys/jbrowse_analytics"
 
 do_start()
 {
-        if [ ! -f "$LOCK_FILE" ] ; then
+        if pgrep $PGREP_ARGS; then
+                echo "$SERVER is already running."
+                RETVAL=1
+	else
                 echo -n $"Starting $SERVER: "
                 cd $ROOT_DIR && ./run.sh 2>&1 | ( while read line; do echo "$(date): ${line}"; done ) >> $LOG_FILE &
                 if [ $? -eq 0 ]; then
-		    sleep 1;
-		    pgrep $PGREP_ARGS > $PIDFILE;
-		    touch $LOCK_FILE;
 		    echo_success;
 		else
 		    echo_failure;
 		fi;
                 echo
-        else
-                echo "$SERVER is already running."
-                RETVAL=1
         fi
 }
 do_stop()
 {
         echo -n $"Stopping $SERVER: "
-        kill `cat "$PIDFILE" 2>/dev/null` >/dev/null 2>&1 && echo_success || echo_failure;
+        pkill $PGREP_ARGS >/dev/null 2>&1 && echo_success || echo_failure;
         RETVAL=$?
 	echo
-        [ $RETVAL -eq 0 ] && rm -f $LOCK_FILE $PIDFILE
-        rm -f "$PIDFILE";
 }
 
 case "$1" in
